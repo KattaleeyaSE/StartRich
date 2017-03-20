81fe98f2c4bd688e4bc4dbafc397979c761c2469
Punjasin <punjasin@gmail.com>
2017-03-20 18:20:26 +0700

    add user fav table

---
+++ database/migrations/2017_03_20_111506_create_user_favorites_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_favorites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('invest_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('invest_id')
+                ->references('id')
+                ->on('investments')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_favorites');
+    }
+}
