7aef91e218f8cb243895284d69c3d31041bd9c9d
Punjasin <punjasin@gmail.com>
2017-03-20 14:33:00 +0700

    nav model

---
+++ database/migrations/2017_03_18_190253_create_investments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvestmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('investments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('amc_id')->unsigned();
+            $table->string('investment_name');
+            $table->string('investment_description');
+$table->integer('invest_type');
+            $table->foreign('amc_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('investments');
+    }
+}
