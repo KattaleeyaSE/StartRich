ce72947989baa00ab8809c6f147b0c8fbfae4184
suriyasasujit2 <suriya_sasujit@cmu.ac.th>
2017-03-20 21:00:49 +0700

    add suitablities models

---
+++ database/migrations/2017_03_20_125910_create_suitability_tests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuitabilityTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suitability_tests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description');
+            $table->integer('amc_id')->unsigned();
+            $table->foreign('amc_id')
+                ->references('id')
+                ->on('amcs')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suitability_tests');
+    }
+}
---
+++ database/migrations/2017_03_20_131734_create_suitability_test_results_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuitabilityTestResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suitability_test_results', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('max_score')->unsigned();
+            $table->integer('min_range')->unsigned();
+            $table->string('type_of_investors')->nullable();
+            $table->integer('suitability_test_id')->unsigned();
+             $table->foreign('suitability_test_id')
+                ->references('id')
+                ->on('suitability_tests')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suitability_test_results');
+    }
+}
