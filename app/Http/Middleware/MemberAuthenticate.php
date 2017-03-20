bc7c8d29beb96c23379bddb13ed722fbc6bc9910
suriyasasujit2 <suriya_sasujit@cmu.ac.th>
2017-03-20 22:39:48 +0700

    adjust middleware

--- app/Http/Middleware/MemberAuthenticate.php
+++ app/Http/Middleware/MemberAuthenticate.php
@@ -3,28 +3,25 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Auth;
 
-class AdminAuthenticate
+class MemberAuthenticate
 {
     /**
      * Handle an incoming request.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure                 $next
-     * @param string|null              $guard
-     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
         if (Auth::guard($guard)->guest()) {
             if ($request->ajax() || $request->wantsJson()) {
                 return response(trans('backpack::base.unauthorized'), 401);
             } else {
-                return redirect()->guest(config('backpack.base.route_prefix', 'admin').'/login');
+                return redirect()->guest('/login');
             }
-        }else if(Auth::check() && is_null(Auth::user()->admin))
+        }else if(Auth::check() && is_null(Auth::user()->member))
         {
             return redirect()->intended('/');
         } 
