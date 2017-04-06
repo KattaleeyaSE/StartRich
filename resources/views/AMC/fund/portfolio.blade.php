64ca74656e18029f60732b8d733a42d3a03f7c03
Punjasin Punya <lunarcraft@Punjasins-iMac.local>
2017-04-06 20:47:16 +0700

    Try chart js

---
+++ resources/views/AMC/fund/portfolio.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('script')
+    <script src="js/libs/Chart.min.js"></script>
+    @endsection
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Portfilio</div>
+
+                    <div class="panel-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
