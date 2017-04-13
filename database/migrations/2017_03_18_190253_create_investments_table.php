b1b6e177ee013bfce2991117cc5533b3915b10c0
Tortrakul <tortrakul.c@hotmail.com>
2017-04-13 23:52:36 +0700

    fix invesments table structure

--- database/migrations/2017_03_18_190253_create_investments_table.php
+++ database/migrations/2017_03_18_190253_create_investments_table.php
@@ -16,41 +16,38 @@ class CreateInvestmentsTable extends Migration
         Schema::create('investments', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('amc_id')->unsigned();
+            $table->foreign('amc_id')->references('id')->on('amcs')->onDelete('cascade')->onUpdate('cascade');
+
+            // about fund
+            $table->string('name', 150);
+            $table->string('code', 50);
+            $table->string('type');
+            $table->string('aimc_type');
+            $table->string('management_company', 150);
+            $table->string('trustee', 150);
+            $table->boolean('payment_policy');
+            $table->string('frequency', 150);
+            $table->string('approved_by', 150);
+            $table->string('supervision', 150);
+            $table->boolean('protected_fund');
+            $table->string('name_of_guarantor', 150);
+            $table->date('fund_start');
+            $table->date('fund_end')->nullable();
             $table->integer('risklevel');
-            $table->date('registered_date');
-            $table->string('Investment_policy');
-$table->integer('assetvalue');
-$table->string('trustee');
-$table->string('frequency');
+            $table->integer('net_asset_value');
 
-            $table->string('name');
-            $table->string('desc');
-            $table->string('company_name');
-            $table->boolean('paymentpolicy');
-$table->integer('type');
-$table->integer('aimcfundtype');
-$table->string('period');
-            $table->string('assetinvest');
-            $table->string('strategy');
-            $table->string('factorimpact');
+            // about investment policy
+            $table->longText('investment_asset_detail');
+            $table->string('strategy_detail');
+            $table->longText('factor_impact');
+            $table->longText('benchmark_detail');
 
-            $table->string('investertype');
-            $table->string('benchmarkdetail');
-            $table->float('stock');
-            $table->float('bond');
-            $table->float('cash');
-            $table->float('assetother');
-$table->date('fund_start');
-$table->date('fund_end');
+            // types of investor
+            $table->longText('type_of_investor');
 
+            //major risk factor
+            $table->longText('major_risk_factor');
 
-
-
-            $table->foreign('amc_id')
-                ->references('id')
-                ->on('users')
-                ->onDelete('cascade')
-                ->onUpdate('cascade');
             $table->timestamps();
         });
 
