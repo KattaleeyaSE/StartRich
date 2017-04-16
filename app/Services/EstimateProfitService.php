ea84cf5704336c2de7bbbc2f8b6789a4005c68a0
suriyasasujit2 <suriya_sasujit@cmu.ac.th>
2017-04-16 13:28:59 +0700

    .

--- app/Services/EstimateProfitService.php
+++ app/Services/EstimateProfitService.php
@@ -29,7 +29,11 @@ class EstimateProfitService implements IEstimateProfitService
         {
             foreach($estimate_profit as $key => $estimate_item)
             {
-                dd($estimate_item);
+                $dividen_history = $estimate_item->devidenHistory;
+                if(sizeof($dividen_history) > 0)
+                {
+                    dd($dividen_history);
+                }
             }
         }
 
