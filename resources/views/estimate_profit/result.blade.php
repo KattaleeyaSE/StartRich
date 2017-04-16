eaea6979c397f3919c3429fd609239ff22235841
suriyasasujit2 <suriya_sasujit@cmu.ac.th>
2017-04-16 16:18:00 +0700

    views

--- resources/views/estimate_profit/result.blade.php
+++ resources/views/estimate_profit/result.blade.php
@@ -9,7 +9,35 @@
 
                 <div class="panel-body"> 
 
-                    
+                    <div class="table-responsive">
+                    <table class="table">
+                            <thead>
+                            <tr> 
+                                <th class="text-center">Buy Date</th> 
+                                <th class="text-center">Fund Name</th> 
+                                <th class="text-center">Company Name</th>  
+                                <th class="text-center">Balance of Investment</th>   
+                            </tr>
+                            </thead>
+                            <tbody> 
+                            @foreach ($result as $item) 
+            
+                                <tr> 
+                                    <td class="text-center">{{$item['estimate_item']->effective_date}}</td>  
+                                    <td class="text-center">
+                                        <strong>{{$item['estimate_item']->fund->aimc_type}}</strong>
+                                        <br>
+                                        {{$item['estimate_item']->fund->name}}
+                                    </td>  
+                                    <td class="text-center">{{$item['estimate_item']->fund->management_company}}</td>   
+                                    <td class="text-center">{{$item['estimate_item']->balance_of_investment}}</td>   
+  
+ 
+                                </tr> 
+                            @endforeach
+                            </tbody>
+                        </table>  
+                    </div>
                     
 
                 </div>
