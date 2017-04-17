cd7b1b688929083f792ba5f92a769e47e96099df
Tortrakul <tortrakul.c@hotmail.com>
2017-04-18 00:06:11 +0700

    fund index show by info

--- resources/views/fund/member/index.blade.php
+++ resources/views/fund/member/index.blade.php
@@ -11,19 +11,27 @@
                     <div class="panel-body">
                         <table class="table">
                             <thead>
-                                <th>Modified date</th>
+                                <th></th>
                                 <th>Fund code</th>
                                 <th>Fund name</th>
                                 <th>Fund normal type</th>
+                                <th>StartRich Rate</th>
+                                <th>Dividend Policy</th>
+                                <th>NAV</th>
+                                <th>Last percentage of return per 1 year</th>
                                 <th>Actions</th>
                             </thead>
                             <tbody>
                                 @foreach($funds as $fund)
                                     <tr>
-                                        <td>{{$fund->updated_at}}</td>
+                                        <td>{!! Form::checkbox('x', 0, 0, []) !!}</td>
                                         <td>{{$fund->code}}</td>
                                         <td>{{$fund->name}}</td>
                                         <td>{{$fund->type}}</td>
+                                        <td>{{$fund->getRate()}}</td>
+                                        <td>{{$fund->payment_policy ? 'YES' : 'NO'}}</td>
+                                        <td>{{$fund->navs->first()->standard}}</td>
+                                        <td>{{ !(is_null($fund->lastPastPerforamce())) ? $fund->lastPastPerforamce()->fundReturn()->oneyear : ''}}</td>
                                         <td>
                                             <a href="{{ route('member.fund.show', $fund->id) }}" class="btn btn-xs btn-info">view</a>
                                         </td>
