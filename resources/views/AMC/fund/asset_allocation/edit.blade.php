a025f3634b9b2c4dda6c4bc70809ab8ba90ce49e
Tortrakul <tortrakul.c@hotmail.com>
2017-05-01 20:12:53 +0700

    validate asset allocation

--- resources/views/AMC/fund/asset_allocation/edit.blade.php
+++ resources/views/AMC/fund/asset_allocation/edit.blade.php
@@ -3,16 +3,63 @@
 
 <div class="row">
 	<div class="container">
-		{!! Form::model($asset_allocation, ['route' => ['amc.fund.update_asset_allocation', $asset_allocation->id], 'method' => 'PATCH', 'class' => 'form-horizontal']) !!}
+		{!! Form::model($asset_allocation, ['route' => ['amc.fund.update_asset_allocation', $asset_allocation->id], 'method' => 'PATCH', 'class' => 'form-horizontal', 'id' => 'form-fund']) !!}
 
-			@include('AMC.fund.asset_allocation.partials._form')
-			
-			<hr>
-
-			{!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+            <h3>Asset Allocation</h3>
+            <section>
+				@include('fund.amc.partials._form-asset-allocation')
+            </section>
 			
 		{!! Form::close() !!}
 	</div>
 </div>
 
+@endsection
+
+@section('script')
+
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-steps/1.1.0/jquery.steps.min.js"></script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+	<script type="text/javascript">
+        var form = $("#form-fund");
+
+        form.validate({
+            errorPlacement: function errorPlacement(error, element) { element.before(error); },
+            rules: {
+                confirm: {
+                    equalTo: "#password"
+                }
+            }
+        });
+        
+		form.steps({
+		    headerTag: "h3",
+		    bodyTag: "section",
+		    transitionEffect: "slideLeft",
+		    autoFocus: true,
+		    onInit: function (event, current) {
+		        $('.actions > ul > li:first-child').attr('style', 'display:none');
+		    },
+            onStepChanging: function (event, currentIndex, newIndex)
+            {
+                form.validate().settings.ignore = ":disabled,:hidden";
+                return form.valid();
+            },
+            onFinishing: function (event, currentIndex)
+            {
+                form.validate().settings.ignore = ":disabled";
+                return form.valid();
+            },
+            onFinished: function (event, currentIndex)
+            {
+            	console.log($(this))
+                $(this).submit();
+            }
+		});
+	</script>
+
+@endsection
+
+@section('style')
+    <link rel="stylesheet" type="text/css" href="/css/form.css">
 @endsection
