05084e5994ea2f2f2c347333904b67afe5ab1a3d
suriyasasujit2 <suriya_sasujit@cmu.ac.th>
2017-05-24 00:01:20 +0700

    rename

---
+++ tests/Unit/SuitabilityServiceTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SuitabilityServiceTest extends TestCase
+{
+    public function testCreateTest_NotNull()
+    {
+
+    }
+
+    public function testCreateTest_Empty()
+    {
+        
+    }
+
+    public function testCreateTest_Null()
+    {
+
+    }   
+}
