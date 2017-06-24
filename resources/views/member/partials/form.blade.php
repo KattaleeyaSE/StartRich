7aee27c946cfedf25d1ff00e9855050fa8ecfb22
Tortrakul <tortrakul.c@hotmail.com>
2017-06-25 02:08:37 +0700

    URS-39: Member can edit their account information which includes first name, last name, password, confirm the password, email, and phone number.

--- resources/views/member/partials/form.blade.php
+++ resources/views/member/partials/form.blade.php
@@ -20,7 +20,8 @@
 <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="firstname">Firstname</label> 
     <div class="col-md-6 col-sm-6 col-xs-12">
-        <input type="text" name="firstname" value="{{isset($member) ? $member->firstname : old('firstname')}}" class="form-control col-md-7 col-xs-12" /> 
+        <input type="text" name="firstname" value="{{isset($member) ? $member->firstname : old('firstname')}}" class="form-control col-md-7 col-xs-12" pattern="^[A-z0-9 ]{1,}$" required/> 
+        <div class="help-block with-errors"></div>
     </div>
     @if ($errors->has('firstname'))
         <div class="row">
@@ -35,7 +36,8 @@
 <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="lastname">Lastname</label> 
     <div class="col-md-6 col-sm-6 col-xs-12">
-        <input type="text" name="lastname" value="{{isset($member) ? $member->lastname : old('lastname')}}" class="form-control col-md-7 col-xs-12" /> 
+        <input type="text" name="lastname" value="{{isset($member) ? $member->lastname : old('lastname')}}" class="form-control col-md-7 col-xs-12" pattern="^[A-z0-9 ]{1,}$" required/> 
+        <div class="help-block with-errors"></div>
     </div>
     @if ($errors->has('lastname'))
         <div class="row">
@@ -50,7 +52,8 @@
 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="email">Email</label> 
     <div class="col-md-6 col-sm-6 col-xs-12">
-        <input type="text" name="email" value="{{isset($member) ? $member->user->email : old('email')}}" class="form-control col-md-7 col-xs-12" /> 
+        <input type="email" name="email" value="{{isset($member) ? $member->user->email : old('email')}}" class="form-control col-md-7 col-xs-12" required/> 
+        <div class="help-block with-errors"></div>
     </div>
     @if ($errors->has('email'))
         <div class="row">
@@ -65,7 +68,8 @@
 <div class="form-group{{ $errors->has('phone_number') ? ' has-error' : '' }}">
     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="password">Phone Number</label> 
     <div class="col-md-6 col-sm-6 col-xs-12">
-        <input type="text" name="phone_number" value="{{isset($member) ? $member->phone_number : old('phone_number')}}" class="form-control col-md-7 col-xs-12" /> 
+        <input type="text" name="phone_number" value="{{isset($member) ? $member->phone_number : old('phone_number')}}" class="form-control col-md-7 col-xs-12" maxlength="10" pattern="^[0-9]{1,}$" required/> 
+        <div class="help-block with-errors"></div>
     </div>
     @if ($errors->has('phone_number'))
         <div class="row">
@@ -80,7 +84,8 @@
 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="password">Password</label> 
     <div class="col-md-6 col-sm-6 col-xs-12">
-        <input type="password" name="password" class="form-control col-md-7 col-xs-12" /> 
+        <input type="password" id="password" name="password" class="form-control col-md-7 col-xs-12" minlength="6" maxlength="15" pattern="^[_A-z0-9]{1,}$"/> 
+        <div class="help-block with-errors"></div>
     </div>
     @if ($errors->has('password'))
         <div class="row">
@@ -95,7 +100,8 @@
 <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="password_confirmation">Confirm Password</label> 
     <div class="col-md-6 col-sm-6 col-xs-12">
-        <input type="password" name="password_confirmation" class="form-control col-md-7 col-xs-12" /> 
+        <input type="password" name="password_confirmation" class="form-control col-md-7 col-xs-12" data-match="#password" /> 
+        <div class="help-block with-errors"></div>
     </div>
     @if ($errors->has('password_confirmation'))
         <div class="row">
