36d368084685a9f025106598e90ca1b3624abb97
Tortrakul <tortrakul.c@hotmail.com>
2017-06-25 02:40:30 +0700

    URS-07: Guest can register as a member to the system by entering the information including username, first name, last name, password, confirm password, email and phone number.When user fulfill the registration information in correct format, system will add user’s information to database.

--- resources/views/member/partials/form.blade.php
+++ resources/views/member/partials/form.blade.php
@@ -2,10 +2,11 @@
     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="username">Username</label> 
     <div class="col-md-6 col-sm-6 col-xs-12">
         @if($submit_text !='Edit')
-        <input type="text" name="username" value="{{isset($member) ? $member->user->username : old('username')}}" class="form-control col-md-7 col-xs-12" /> 
+        <input type="text" name="username" value="{{isset($member) ? $member->user->username : old('username')}}" class="form-control col-md-7 col-xs-12" required/> 
         @else
          <input type="text" name="username" value="{{isset($member) ? $member->user->username : old('username')}}" class="form-control col-md-7 col-xs-12" readonly/> 
         @endif
+        <div class="help-block with-errors"></div>
     </div>
     @if ($errors->has('username'))
         <div class="row">
