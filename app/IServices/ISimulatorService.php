250f37d75ad77dc45b2fd70f136f4ae4b0ef34aa
suriyasasujit2 <suriya_sasujit@cmu.ac.th>
2017-07-17 20:34:49 +0700

    add return type descriptin

--- app/IServices/ISimulatorService.php
+++ app/IServices/ISimulatorService.php
@@ -9,8 +9,12 @@ interface ISimulatorService
     /**
      * @param Request $request
      *
-     * @return array $results
-    **/     
+     * @return Collection consists of
+     * 'remaining_unit' => $remaining_unit,
+     * 'return_profit_total' => $return_profit_total,
+     * 'return_profit' => $return_profit
+     * 'return_profit_percent' => $return_profit_percent
+    **/          
     public function create_simulator(Request $request); 
  
 }
